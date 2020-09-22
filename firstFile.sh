c1b93259e4109b57e87e214dc44aa0da4082b7fa
Nikitha <nikiishi14@gmail.com>
2020-09-22 20:23:47 +0530

    adding subtracting to the first file

--- firstFile.sh
+++ firstFile.sh
@@ -5,3 +5,9 @@ echo "Enter First number"
 read x
 echo "Enter Second Number"
 read y
+#2nd commit
+echo "Addition of two numbers"
+echo $((x+y));
+#3rd commit
+echo "Subtraction of two numbers"
+echo $((x-y));
